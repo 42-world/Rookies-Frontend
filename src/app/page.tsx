402277de4ff3c98cae5b661072283ef9f9e5ace8
chichoon <chichoon.choi@gmail.com>
2023-05-21 10:55:58 +0900

    chore: getMe services로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query";
 
-async function getMe() {
-  // TODO: axios로 변경
-  const res = await fetch("http://localhost:8888/users/me", {
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data = res.json();
-
-  if (res.status !== 200) {
-    throw new Error();
-  }
-
-  return data;
-}
+import { getMe } from "@/services";
 
 export default function Page() {
   const { data, isError } = useQuery(["me"], () => getMe(), {
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export { getMe } from "./getMe";
