9e9a7a3918dd637e27f677dd72d642854f197138
raejun92 <cvcv1324@naver.com>
2023-05-21 12:20:59 +0900

    feature: 메인 articles 데이터 fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,48 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 
 import { getMe } from "@/services";
 
+async function getArticles() {
+  const res = await fetch("http://localhost:8888/articles?order=DESC&page=1&take=10&categoryId=1", {
+    credentials: "include",
+  });
+
+  const data = res.json();
+
+  if (res.status !== 200) {
+    throw new Error();
+  }
+
+  return data;
+}
+
 export default function Page() {
-  const { data, isError } = useQuery(["me"], () => getMe(), {
-    retry: false,
+  const [{ data: myData, isError }, { data: articleData }] = useQueries({
+    queries: [
+      { queryKey: ["me"], queryFn: () => getMe() },
+      {
+        queryKey: ["articles"],
+        queryFn: () => getArticles(),
+      },
+    ],
   });
 
   return (
-    <div>
+    <>
       <h1>Rookies 홈이지롱!!</h1>
-      {isError ? (
-        <pre>에러 ㅋ</pre>
-      ) : (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      )}
-    </div>
+      {isError ? <pre>에러 ㅋ</pre> : <pre>{JSON.stringify(myData, null, 2)}</pre>}
+      {articleData &&
+        articleData.data.map((article: any) => (
+          <ul key={article.id}>
+            <li>{article.id}</li>
+            <li>{article.title}</li>
+            <li>{article.content}</li>
+            <li>{article.createdAt}</li>
+            <li>{article.updatedAt}</li>
+          </ul>
+        ))}
+    </>
   );
 }
