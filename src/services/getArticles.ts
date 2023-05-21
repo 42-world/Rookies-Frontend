2af6464f3d605b5959b84fb8eb1f7c7c9a34f513
Seojunhwan <seojunhwan@kakao.com>
2023-05-21 23:36:13 +0900

    refactor: getArticles date 타입 변환

--- src/services/getArticles.ts
+++ src/services/getArticles.ts
@@ -10,11 +10,20 @@ interface GetArticlesParams {
 
 type GetArticlesResponse = WithPageMeta<Article[]>;
 
-export async function getArticles(params: GetArticlesParams) {
+export async function getArticles(
+  params: GetArticlesParams
+): Promise<GetArticlesResponse> {
   // NOTE: Params 중 number 타입의 값은 URLSearchParams를 사용하면 자동으로 문자열로 변환됩니다.
   const { data, meta } = await api.get<GetArticlesResponse>(
     `http://localhost:8888/articles?${new URLSearchParams(params as any)}`
   );
 
-  return data;
+  return {
+    data: data.map((article) => ({
+      ...article,
+      createdAt: new Date(article.createdAt),
+      updatedAt: new Date(article.updatedAt),
+    })),
+    meta,
+  };
 }
