318b5c95426e9bc3e491ec64bd08425900bd35a9
hyongti <dev.hyongti@gmail.com>
2023-05-21 10:38:27 +0900

    [Resolve #4] tailwindcss 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import { ReactNode } from "react";
-import Provider from "@/libs/provider";
+import { ReactNode } from 'react';
+import Provider from '@/libs/provider';
+import './global.css';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <h1>Rookies</h1>
+        <h1 className="text-red-500">Rookies</h1>
         <Provider>{children}</Provider>
       </body>
     </html>
