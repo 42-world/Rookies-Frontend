317705fbed9250a3858f627bf454fd558d467b43
Seojunhwan <seojunhwan@kakao.com>
2023-05-21 23:59:31 +0900

    feat: useInfinityQueryWithMeta

---
+++ src/app/board/useInfinityQueryWithMeta.ts
@@ -0,0 +1,23 @@
+import {
+  QueryFunction,
+  QueryKey,
+  UseInfiniteQueryOptions,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import type { WithPageMeta } from "@/interfaces/article";
+
+export const useInfinityQueryWithMeta = <TData extends WithPageMeta<{}>>(
+  queryKey: QueryKey,
+  fetchFn: QueryFunction<TData>,
+  config?: UseInfiniteQueryOptions<TData>
+) => {
+  return useInfiniteQuery(queryKey, fetchFn, {
+    getNextPageParam: (lastPage) => {
+      return lastPage.meta.hasNextPage && lastPage.meta.page + 1;
+    },
+    getPreviousPageParam: (firstPage) => {
+      return firstPage.meta.hasPreviousPage && firstPage.meta.page - 1;
+    },
+    ...config,
+  });
+};
