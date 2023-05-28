cd9a75df5efd702876e4f2d2b14b84b5b8722498
chichoon <chichoon.choi@gmail.com>
2023-05-28 15:06:28 +0900

    feat: 내 정보 받아오는 함수들 수정

--- src/services/getMe.ts
+++ src/services/getMe.ts
@@ -1,3 +1,12 @@
+import { Article, Comment, WithPageMeta } from "@/interfaces/article";
+import { api } from "@/libs/fetch";
+
+interface Params {
+  order?: "ASC" | "DESC";
+  page?: number;
+  take?: number;
+}
+
 export async function getMe() {
   // TODO: axios로 변경
   const res = await fetch("https://api-alpha.42world.kr/users/me", {
@@ -13,4 +22,42 @@ export async function getMe() {
   }
 
   return data;
+}
+
+async function getMyData<T extends Article | Comment>(
+  params: Params,
+  path: string
+): Promise<WithPageMeta<T[]>> {
+  const { data: dataList, meta } = await api.get<WithPageMeta<T[]>>(
+    `https://api-alpha.42world.kr/users/me/${path}${new URLSearchParams(
+      params as Record<string, string>
+    )}`
+  );
+
+  return {
+    data: dataList.map((data) => ({
+      ...data,
+      createdAt: new Date(data.createdAt),
+      updatedAt: new Date(data.updatedAt),
+    })),
+    meta,
+  };
+}
+
+export async function getMyComments(
+  params: Params = {}
+): Promise<WithPageMeta<Comment[]>> {
+  return getMyData<Comment>(params, "comments");
+}
+
+export async function getMyArticles(
+  params: Params = {}
+): Promise<WithPageMeta<Article[]>> {
+  return getMyData<Article>(params, "articles");
+}
+
+export async function getMyLikedArticles(
+  params: Params = {}
+): Promise<WithPageMeta<Article[]>> {
+  return getMyData<Article>(params, "like-articles");
 }
