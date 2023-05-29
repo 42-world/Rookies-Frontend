117f979aee9825ad1fbc79150fca79b727915353
hyongti <dev.hyongti@gmail.com>
2023-05-29 13:14:47 +0900

    feat: 401 에러 시 auth로 리다이렉트

--- src/libs/provider.tsx
+++ src/libs/provider.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function Provider({ children }: React.PropsWithChildren) {
-  const [client] = useState(new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } }));
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: 0,
+          staleTime: 5000,
+          useErrorBoundary: (error) => (error as Error).message === '401',
+        },
+      },
+    })
+  );
 
   return (
     <QueryClientProvider client={client}>
