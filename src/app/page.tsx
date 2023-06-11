c1d531625cc8c78c7ec4be2de36a157d3576fe74
raejun92 <cvcv1324@naver.com>
2023-06-11 12:46:29 +0900

    feature: 메인 페이지 csr에서 ssr로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,38 @@
-'use client';
-
-import { useQueries } from '@tanstack/react-query';
+import { cookies } from 'next/headers';
+import { Category } from '@/interfaces/article';
 import { getArticles } from '@/services/getArticles';
+import { getCategories } from '@/services/getCategories';
 
-export default function Page() {
-  const [{ data: articleData }] = useQueries({
-    queries: [
-      // { queryKey: ['me'], queryFn: () => getMe() },
-      {
-        queryKey: ['articles'],
-        queryFn: () => getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 1 }),
-      },
-    ],
-  });
+export default async function Page() {
+  const cookieHeader = cookies().toString();
+  const categoryList = await getCategories({ cookieHeader });
+  // const { data: freeArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 1 });
+  // const { data: anonymousArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 2 });
+  // const { data: noticeArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 4 });
+  // const { data: feedbackArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 5 });
+  // const { data: jobArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 7 });
 
   return (
     <>
       <h1>Rookies 홈이지롱!!</h1>
-      {articleData &&
-        articleData.data.map((article: any) => (
-          <ul key={article.id}>
-            <li>{article.id}</li>
-            <li>{article.title}</li>
-            <li>{article.content}</li>
-            {/* <li>{article.createdAt}</li>
-            <li>{article.updatedAt}</li> */}
-          </ul>
-        ))}
+
+      <div className='flex gap-3'>
+        {/* @ts-expect-error Async Server Component */}
+        <CategoryList categoryList={categoryList} />
+      </div>
+    </>
+  );
+}
+
+async function CategoryList({ categoryList }: { categoryList: Category[] }) {
+  return (
+    <>
+      {categoryList.map(({ id, name }) => (
+        <ul key={id}>
+          <li>{id}</li>
+          <li>{name}</li>
+        </ul>
+      ))}
     </>
   );
 }
