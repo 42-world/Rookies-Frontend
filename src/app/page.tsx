459b2ef8460c8ec5b35dcd954366a0719362f1e0
raejun92 <cvcv1324@naver.com>
2023-06-18 10:39:02 +0900

    fix: comment 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ import { cookies } from 'next/headers';
 import { getCategories } from '@/services/getCategories';
 
 export default async function Page() {
-  // const { data: freeArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 1 });
-  // const { data: anonymousArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 2 });
-  // const { data: noticeArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 4 });
-  // const { data: feedbackArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 5 });
-  // const { data: jobArticleList } = await getArticles({ order: 'DESC', page: 1, take: 10, categoryId: 7 });
-
   return (
     <>
       <h1>Rookies 홈이지롱!!</h1>
